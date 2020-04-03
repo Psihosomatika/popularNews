ad3b3462f3c24cad46d8941e89b6029d40720bfc
Diana Rukavishnikova <psihomatika@gmail.com>
2020-04-03 15:32:12 +0300

    Предварительная настройка вебпака, установка всех программ, необходимых для верстки и сборки

---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+const path = require('path');//+
+const HtmlWebpackPlugin = require('html-webpack-plugin');//+
+const WebpackMd5Hash = require('webpack-md5-hash');//+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');//+
+const webpack = require('webpack');
+const cssnano = require('cssnano');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');//+
+
+const isDev = process.env.NODE_ENV === 'development';//+
+
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    articles: './src/saved-articles/articles.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './js/[name].[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          isDev
+            ? 'style-loader'
+            : {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../',
+              },
+            },
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          'file-loader?name=./images/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 85,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [
+                  0.7,
+                  0.9,
+                ],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './css/[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,//возможно удалить? 
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: [
+        'index',
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/saved-articles/articles.html',
+      filename: 'articles.html',
+      chunks: [
+        'articles',
+      ],
+    }),
+
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+      cssProcessorPluginOptions: {
+        preset: [
+          'default',
+        ],
+      },
+      canPrint: true,
+    }),
+  ],
+};
