bf06201a952d01b0640263e281ba72a99aca7c99
Diana Rukavishnikova <psihomatika@gmail.com>
2020-04-16 15:18:33 +0300

    Исправлены ошибки со скролом, улучшена адаптивная верстка отдельных элементов

--- src/about.js
+++ src/about.js
@@ -3,9 +3,11 @@ import Flickity from 'flickity';
 
 const slider = new Flickity('.carousel', {
   // Настройки плагина
-  cellAlign: 'centr',
-  contain: true,
-  freeScroll: true,
   wrapAround: true,
-  groupCells: '90%',
+
 });
+/*  cellAlign: 'centr',
+contain: true,
+freeScroll: true,
+wrapAround: true,
+groupCells: '90%',*/
