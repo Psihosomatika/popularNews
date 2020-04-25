a8c37459f1b7871a6b0eed90c9af94f6cedb6440
Diana Rukavishnikova <psihomatika@gmail.com>
2020-04-25 14:30:34 +0300

    Updates

--- src/js/components/commitCardList.js
+++
@@ -1,9 +0,0 @@
-export default class CommitCardList {
-  constructor(list) {
-    this.list = list;
-    this.renderCards = this.renderCards.bind(this);
-  }
-  renderCards(caruselCell) {
-    this.list.append(caruselCell);
-  }
-}
--- src/js/modules/githubApi.js
+++
@@ -1,9 +0,0 @@
-export default class GithubApi {
-  constructor(url) {
-    this.url = url;
-  }
-  async getCommits() {
-    const res = await fetch(this.url);
-    return await (res.ok ? Promise.resolve(res.json()) : Promise.reject(`Ошибка: ${res.status}`));
-  }
-}
