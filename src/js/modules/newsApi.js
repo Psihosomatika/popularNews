5ff3254554916545fc7658e17216361e1548459c
Diana Rukavishnikova <psihomatika@gmail.com>
2020-07-27 16:59:33 +0300

    изменения в апи

--- src/js/modules/newsApi.js
+++ src/js/modules/newsApi.js
@@ -15,16 +15,6 @@ export default class NewsApi {
     const toDate = currentDate.toISOString().slice(0,10);
 
     const res = await fetch(`${this.baseUrl}${searchWord}&from=${fromDate}&to=${toDate}&language=ru&${this.parameters}`);
-    //fetch(`${this.options.baseUrl}q=${phrase}&from=${weekAgo}&to=${currentDate}&pageSize=100&language=${this.options.lang}&sortBy=publishedAt&apiKey=${this.options.apiKey}
-    /*const api = new NewsApi({
-  baseUrl: 'https://newsapi.org/v2/everything?',
-  apiKey: 'e0d4edaf7bc24c56befea2a3b9e45f82',
-  lang: 'ru'
-  })
-  export const URL_START = 'https://newsapi.org/v2/everything?q=';
-export const URL_END = 'sortBy=popularity&pageSize=100&apiKey=da509dbac2d74492a4498c044ca28d18';
-  */
-
     return await (res.ok ? Promise.resolve(res.json()) : Promise.reject(`Ошибка: ${res.status}`));
 
   }
