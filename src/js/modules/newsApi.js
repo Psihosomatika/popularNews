86af90f0ea68bdc7c69f5f511c60f51769944cc5
Diana Rukavishnikova <psihomatika@gmail.com>
2020-07-30 15:15:08 +0300

    исправлена ошибка с CORS

--- src/js/modules/newsApi.js
+++ src/js/modules/newsApi.js
@@ -8,13 +8,14 @@ export default class NewsApi {
   }
 
   async getNews(searchWord) {
+    const proxyurl = 'https://cors-anywhere.herokuapp.com/';
     const currentDate = new Date();
     const previousDateInMs = 6 * MS_IN_DAY;
     const previousDate = new Date(currentDate.getTime()-previousDateInMs);
     const fromDate = previousDate.toISOString().slice(0, 10);
     const toDate = currentDate.toISOString().slice(0,10);
 
-    const res = await fetch(`${this.baseUrl}${searchWord}&from=${fromDate}&to=${toDate}&language=ru&${this.parameters}`);
+    const res = await fetch(`${proxyurl}` + `${this.baseUrl}${searchWord}&from=${fromDate}&to=${toDate}&language=ru&${this.parameters}`);
     return await (res.ok ? Promise.resolve(res.json()) : Promise.reject(`Ошибка: ${res.status}`));
 
   }
